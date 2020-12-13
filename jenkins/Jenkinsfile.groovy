e81ab406c67796ab05860f6d5bbaf9b02f4c5453
MariaMorozov <maria.morozov@gmail.com>
2020-12-13 15:43:04 +0200

    Jenkinsfile2

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -4,6 +4,14 @@ pipeline {
     agent any
 
     stages {
+        stage('Input') {
+            steps {
+                script {
+                    userInput = input message: 'Please provide your input', ok: 'confirm', parameters: [choice(name: '', choices: ['option 1', 'option2'], description: '')]
+                }
+            }
+
+        }
         stage('Hello') {
             steps {
                 dir('CoolNewDirectory') {
@@ -15,6 +23,11 @@ pipeline {
         stage('Check Working Directory') {
             steps {
                 sh "pwd"
+            }
+        }
+        stage('Print Inputed string') {
+            steps {
+                println("Input was " + userInput)
             }
         }
     }
