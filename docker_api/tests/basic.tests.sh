e4445b4dd89f43b4d7ef5797e82c6be04040a848
MariaMorozov <maria.morozov@gmail.com>
2020-12-13 17:16:32 +0200

    Test file

--- docker_api/tests/basic.tests.sh
+++ docker_api/tests/basic.tests.sh
@@ -1,1 +1,22 @@
-pwd
+#!/bin/bash
+check_result() {
+  RESULT=$?
+  MESSAGE=S1
+  if [ $RESULT == 0 ]; then
+    echo [ SUCCESS ] $MESSAGE
+  else
+    echo [ FAIL ] $MESSAGE
+    exit 1
+  fi
+}
+
+for city in 'Modiin' 'New York'; do
+ docker run MyPythonProject -c $city | grep longitude
+ check_result longitude
+
+ docker run MyPythonProject - c $city |grep weather
+ check_result weather
+
+done
+
+exit 0
