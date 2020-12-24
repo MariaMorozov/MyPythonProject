1d6e0e08d9119f165508cfc5c6062d5837d8f2e5
MariaMorozov <maria.morozov@gmail.com>
2020-12-24 16:02:04 +0200

    deployment

--- docker_api/tests/basic.tests.sh
+++ docker_api/tests/basic.tests.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 check_result() {
   RESULT=$?
-  MESSAGE=S1
+  MESSAGE1=$1
+  MESSAGE2=$2
   if [ $RESULT == 0 ]; then
-    echo [ SUCCESS ] $MESSAGE
+    echo [ SUCCESS ] $MESSAGE1 $MESSAGE2
   else
-    echo [ FAIL ] $MESSAGE
+    echo [ FAIL ] $MESSAGE1 $MESSAGE2
     exit 1
   fi
 }
 
 for city in 'Modiin' 'Moscow'; do
  sudo docker run mypythonproject -c $city | grep "lon"
- check_result "lon"
+ check_result $city lontitude
  sudo docker run mypythonproject -c $city | grep "weather"
- check_result "weather"
+ check_result $city weather
 done
 
 exit 0
--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -19,21 +19,12 @@ pipeline {
             steps {
                 dir('docker_api/tests') {
                     script{
-                        sh "sudo chmod 777 basic.tests.sh"
+                        sh "sudo chmod 755 basic.tests.sh"
                         sh "./basic.tests.sh"
                     }
                 }
             }
         }
-        stage('Upload image to repository') {
-            steps {
-                sh "pwd"
-            }
-        }
-        stage('Print Inputed string') {
-            steps {
-                println("Empty " )
-            }
-        }
-    }
+
+     }
 }
