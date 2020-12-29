fef02a8bc7968f9ebf856dcdec5607d472da4c08
MariaMorozov <maria.morozov@gmail.com>
2020-12-29 14:56:40 +0200

    test

--- docker_api/tests/basic.tests.sh
+++ docker_api/tests/basic.tests.sh
@@ -3,12 +3,12 @@ TAG=$1
 check_result() {
   RESULT=$?
   MESSAGE=$1
-  SECOND-MESSAGE=$2
+
 
   if [ $RESULT == 0 ]; then
-    echo [SUCCESS] $MESSAGE $SECOND-MESSAGE
+    echo [SUCCESS] $MESSAGE
   else
-    echo [FAIL] $MESSAGE $SECOND-MESSAGE
+    echo [FAIL] $MESSAGE
     exit 1
   fi
 }
@@ -16,11 +16,11 @@ RUN="sudo docker run $TAG"
 #sudo docker run $TAG | grep "lon"
 for city in 'Modiin' 'Moscow'; do
   $RUN -c $city | grep $city
-  check_result "city_name" $city
+  check_result "city_name $city"
   $RUN -c $city | grep "lon"
-  check_result $city "lon"
+  check_result "$city lon"
   $RUN -c $city | grep "weather"
-  check_result $city "weather"
+  check_result "$city weather"
 
 done
 
