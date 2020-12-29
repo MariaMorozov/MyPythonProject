1e6636996e697d4a88ee3570e6e5a50067d53b37
MariaMorozov <maria.morozov@gmail.com>
2020-12-29 14:49:45 +0200

    test

--- docker_api/main.py
+++ docker_api/main.py
@@ -30,7 +30,7 @@ def main():
     logger.info("Parameters passed are " + str(args))
     my_response = send.send(load_key(), load_host(), city)
 
-    # print(my_response)
+    print(my_response)
 
 
 if __name__ == "__main__":
