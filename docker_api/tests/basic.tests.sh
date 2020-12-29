cd24322aac2f4e94e71a6d85feaddb0b5ed58fb1
MariaMorozov <maria.morozov@gmail.com>
2020-12-29 14:32:50 +0200

    test

--- docker_api/tests/basic.tests.sh
+++ docker_api/tests/basic.tests.sh
@@ -11,19 +11,16 @@ check_result() {
     exit 1
   fi
 }
-
+RUN = "sudo docker run $TAG"
 #sudo docker run $TAG | grep "lon"
 for city in 'Modiin' 'Moscow'; do
+  $RUN -c $city | grep $city
+  check_result $city
   sudo docker run $TAG -c $city | grep "lon"
   check_result "lon"
   sudo docker run $TAG -c $city | grep "weather"
   check_result "weather"
-  if [ sudo docker run $TAG -c $city  ['city_name'] = $city ]
-  then
-  check_result $city
-  else
-  echo "City name problem"
-  fi
+
 done
 
 exit 0
