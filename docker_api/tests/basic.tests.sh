e125211818a7b0387192343c8ebcacf5fd2c7036
MariaMorozov <maria.morozov@gmail.com>
2021-01-03 15:01:26 +0200

    test

--- docker_api/tests/basic.tests.sh
+++ docker_api/tests/basic.tests.sh
@@ -18,11 +18,11 @@ containerName=$(echo $TAG | cut -d'-' -f2)
 sudo docker rm -f $(sudo docker ps -aq)
 echo "removed"
 sudo docker run -d -p 5000:5000 --name $containerName $TAG
-echo "run"
+echo "run $containerName"
 sleep 5
 for city in 'Modiin' 'Moscow'; do
   echo $city
-  curl -s -X POST --header "Content-Type: application/json" --data '{"city": '$city'}' http://localhost:5000 | grep $city
+  curl -s -X POST --header "Content-Type: application/json" --data '{"city_name": "'$city'"}' http://localhost:5000 | grep $city
   check_result $city
 
 #RUN="sudo docker run $TAG"
