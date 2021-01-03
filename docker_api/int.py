06a311c3153a4285802de738479f07c8fb5fcb76
MariaMorozov <maria.morozov@gmail.com>
2021-01-03 12:48:17 +0200

    int.py

--- docker_api/int.py
+++ docker_api/int.py
@@ -4,3 +4,28 @@ import json
 import send
 
 app = Flask(__name__)
+
+
+def load_config():
+    with open('./configurations.json') as json_file:
+        configs = json.load(json_file)
+        key = configs['configurations']['key']
+        host = configs['configurations']['host']
+        return key, host
+
+
+@app.route('/', methods=['POST'])
+def main():
+    data = request.get_json() or {}
+    city = data['city']
+    key = load_config()[0]
+    host = load_config()[1]
+    response = send.send(key, host, city)
+    json_response = json.loads(response)
+
+    # this can be used to custom construct json response
+    # data = {}
+    # data['key'] = 'value'
+    # json_data = json.dumps(data)
+
+    return json_response['sentiment']
